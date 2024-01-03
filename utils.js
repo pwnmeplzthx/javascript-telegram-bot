6129aadc1d50f86c1b1079681f8193e73fb85bf4
pwnmeplzthx <pshumakov11@mail.ru>
2024-01-03 16:55:05 +0300

    setting up bot responses

---
+++ utils.js
@@ -0,0 +1,35 @@
+const questions = require('./questions.json');
+const { Random } = require('random-js')
+
+const getRandomQuestion = (topic) => {
+    const random = new Random();
+
+    let questionTopic = topic.toLowerCase();
+    if (questionTopic === 'случайный вопрос') {
+        questionTopic = Object.keys(questions)[
+            random.integer(0, Object.keys(questions).length - 1)
+        ]
+    }
+    // const randomIndex = Math.floor(Math.random() * questions[questionTopic].length);
+    const randomIndex = random.integer(0, questions[questionTopic].length - 1)
+
+    return {
+        question: questions[questionTopic][randomIndex],
+        questionTopic,
+    };
+};
+
+const getCorrectAnswer = (topic, id) => {
+    const question = questions[topic.toLowerCase()].find((question) => question.id === id);
+
+    if (!question.hasOptions) {
+        return question.answer;
+    }
+
+    return question.options.find((option) => option.isCorrect).text;
+}
+
+module.exports = {
+    getRandomQuestion,
+    getCorrectAnswer
+}
